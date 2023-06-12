a9febfed934e5830961d5b1af96d79d632a946a4
Alexander <alrlucano@gmail.com>
2023-06-12 12:01:06 -0500

    Creamos todas las carpetas y files , creamos las entidades y codigo

--- src/main/java/com/tutorial/crud/controller/ProductoController.java
+++ src/main/java/com/tutorial/crud/controller/ProductoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/producto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("*")
 public class ProductoController {
 
     @Autowired
