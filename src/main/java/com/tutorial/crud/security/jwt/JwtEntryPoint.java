64f2eed174bc816a7ee0214a91313e4dcba18e6f
Alexander <alrlucano@gmail.com>
2023-06-12 12:42:49 -0500

    Codigo de JWT carpeta

--- src/main/java/com/tutorial/crud/security/jwt/JwtEntryPoint.java
+++ src/main/java/com/tutorial/crud/security/jwt/JwtEntryPoint.java
@@ -1,4 +1,21 @@
 package com.tutorial.crud.security.jwt;
 
-public class JwtEntryPoint {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+    private final static Logger logger= LoggerFactory.getLogger(JwtEntryPoint.class);
+    @Override
+    public void commence(HttpServletRequest req, HttpServletResponse resp, AuthenticationException e) throws IOException, ServletException {
+        logger.error("fail en el método commence");
+        resp.sendError(HttpServletResponse.SC_UNAUTHORIZED,"No autorizado");
+    }
+
 }
